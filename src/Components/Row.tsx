70eb5e3eb944f511ddcf0c0323eab05ac06e4974
Fillipi Pereira <wenanzer@gmx.de>
2021-04-30 18:56:03 +0200

    Stilezed

--- src/Components/Row.tsx
+++ src/Components/Row.tsx
@@ -5,16 +5,34 @@ export const Row = ({
   handleDeleteTodo,
   handleCheckTodo,
 }: TodoProps) => (
-  < div >
-    <p>{task}</p>
-    <div>
-      <button aria-label="Delete a todo" onClick={() => handleDeleteTodo(id)}>
+  < div
+    className={`
+        flex w-full p-4 mb-2 justify-between items-center
+        ${isCompleted ? "bg-gray-400" : "bg-green-300"}
+      `}
+  >
+    <p
+      className={`
+          m1-2 text-xl font-sans font-medium
+          ${isCompleted ? "text-white line-through" : "text-gray-700"}
+        `}
+    >
+      {task}
+    </p>
+    <div className="m-1/6 flex justify-between items-center mr-2">
+      <button
+        aria-label="Delete a todo"
+        className="h-7 w-7 flex justify-center items-center bg-red-400 hover:bg-red-500 text-white font-bold rounded"
+        onClick={() => handleDeleteTodo(id)}
+      >
         X
       </button>
       <input
         type="checkbox"
         checked={isCompleted}
-        onChange={() => handleCheckTodo(id)} />
+        onChange={() => handleCheckTodo(id)}
+        className="form-checkbox h-7 w-7"
+      />
     </div>
   </div >
 )
